e158271ecacd0b26ddccdc79617978b1b411afa9
rivervil16 <agustinriverovila@gmail.com>
2022-09-01 01:52:36 +0000

    fin de generacion carta aleatoria

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function(number) {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let simbolo = ["J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let random = Math.floor(Math.random() * (simbolo.length - 1));
+  document.querySelector("#numero").append(simbolo[random]);
+
+  let iconos = ["♦", "♥", "♠", "♣"];
+  let randomIconos = Math.floor(Math.random() * (iconos.length - 1));
+  document.querySelector("#palo").append(iconos[randomIconos]);
+  document.querySelector("#palo2").append(iconos[randomIconos]);
+  if (iconos[randomIconos] === "♦" || iconos[randomIconos] === "♥") {
+    document.querySelector("#palo").style.color = "red";
+    document.querySelector("#palo2").style.color = "red";
+  }
 };
